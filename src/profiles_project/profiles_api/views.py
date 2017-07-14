2ef04792743c3f882dc8b354b57c807d362e0076
mac <mac@172-13-131-35.lightspeed.cicril.sbcglobal.net>
2017-07-14 14:43:34 +0800

    Added profile listing and creating feature.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from rest_framework import viewsets
 
 from . import serializers
+from . import models
 
 # Create your views here.
 class HelloApiView(APIView):
@@ -111,6 +112,13 @@ class HelloViewSet(viewsets.ViewSet):
 
         return Response({'http_method', 'DELETE'})
         
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, creating and updating profile."""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    
 
 
 
