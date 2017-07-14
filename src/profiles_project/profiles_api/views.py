e15797a908b03071e5071f948d22edab4a094cf4
mac <mac@macs-MacBook-Pro.local>
2017-07-14 22:47:17 +0800

    Added improved permissions to user profile feed items api.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -8,7 +8,9 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from . import serializers
 from . import models
@@ -138,8 +140,26 @@ class LoginViewSet(viewsets.ViewSet):
 
         return ObtainAuthToken().post(request)
         # return Response({'name':'name'})
-
         # return obtain_auth_token(request)
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items."""
+
+    authentication_classes = (TokenAuthentication, )
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    # permission_classes = (permissions.PostOwnStatus, IsAuthenticatedOrReadOnly)
+    permission_classes = (permissions.PostOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in users."""
+
+        serializer.save(user_profile=self.request.user)
+
+
+
+
 
 
 
